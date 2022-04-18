d21224cdfc42ef4f0a97ad4a8233183ab2eb6bbb
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-04-18 17:13:55 +0300

    commit 2

--- print_uint.c
+++ print_uint.c
@@ -9,7 +9,7 @@ int print_uint(va_list args, char *buffer, int *arrlength)
 {
 	unsigned int i = 0, j = 1, z, l = 0;
 	int sum = 0;
-	*if (!i)
+	if (!i)
 		return buff_push(buffer,'0', arrlength);
 	i = va_arg(args, int);
 	z = i;
